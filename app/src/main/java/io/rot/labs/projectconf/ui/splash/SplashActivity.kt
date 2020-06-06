365c7684281f5ab7d095cbdbc7bb91496474beb8
Rohan Maity <rmrohan03@gmail.com>
2020-06-07 00:49:15 +0530

    Add Splash

---
+++ app/src/main/java/io/rot/labs/projectconf/ui/splash/SplashActivity.kt
@@ -0,0 +1,27 @@
+package io.rot.labs.projectconf.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import io.rot.labs.projectconf.R
+import io.rot.labs.projectconf.ui.main.MainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 2500)
+    }
+}
